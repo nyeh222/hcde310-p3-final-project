5f279eb4e0b5e1cf81acd3825467d69f38e51f8d
Nox Yeh <nyeh2@uw.edu>
2025-12-10 23:07:50 -0800

    add quote to database and add per task

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from projectsecrets import app_secrets
+from functions import all_quotes
+import random
 
 app = Flask(__name__)
 app.secret_key = app_secrets.app_key
@@ -13,6 +15,8 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     deadline = db.Column(db.String)
     tasktext = db.Column(db.String, nullable=False)
+    quote = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean)
 
 with app.app_context():
@@ -21,9 +25,12 @@ with app.app_context():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        random_quote = random.choice(all_quotes())
         taskItem = Task(
             tasktext = request.form['task'],
             deadline = request.form['deadline'],
+            quote = random_quote[0],
+            author = random_quote[1],
             completed = False
         )
         db.session.add(taskItem)
@@ -31,10 +38,12 @@ def index():
         return redirect('/')
     else:
         tasklist = db.session.execute(db.select(Task).order_by(Task.deadline)).scalars()
+        # random_quote = random.choice(all_quotes())
         return render_template('index.html', tasklist=tasklist)
 
 @app.route('/completed', methods=['GET', 'POST'])
 def completed():
+    tasklist = db.session.execute(db.select(Task).where(Task.completed == False).order_by(Task.deadline)).scalars()
     if request.method == 'POST':
         marked_task = request.form['select']
         task = db.session.execute(db.select(Task).where(Task.id == marked_task)).scalar()
@@ -43,4 +52,5 @@ def completed():
         return redirect('/completed')
     else:
         taskdone = db.session.execute(db.select(Task).where(Task.completed == True).order_by(Task.deadline)).scalars()
-        return render_template('completed.html', taskdone=taskdone)
+        return render_template('completed.html', taskdone=taskdone, tasklist=tasklist)
+
